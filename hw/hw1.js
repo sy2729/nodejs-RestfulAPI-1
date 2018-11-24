3b115559ea1acd4097893db0b0ce05f3fa45b9fc
Shuai Yuan <shuaiyuan@Shuais-MBP.fios-router.home>
2018-11-24 10:39:41 -0500

    finished first homework

--- hw/hw1.js
+++ hw/hw1.js
@@ -5,13 +5,32 @@ const url = require('url');
 var server = http.createServer(function(req, res) {
 
   let parsedUrl = url.parse(req.url, true);
+  let {pathname, query} = parsedUrl;
+  pathname = pathname.replace(/^\/+|\/+$/g, '')
+  // 
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  // 
 
   req.on('data', function(e) {
-    console.log(a)
+    
   })
   req.on('end', function() {
-    console.log(parsedUrl)
-    res.end('hello world')
+
+     let chosenHandlers = typeof (routers[pathname]) !== 'undefined' ? routers[pathname] : handlers.notFound;
+     var data = {
+      'trimmedPath': pathname,
+      'query': query,
+    };
+
+     chosenHandlers(
+       data, 
+       function(statusCode, payload){
+          statusCode = typeof statusCode === 'number'? statusCode : 200;
+          payload = typeof payload === 'object'? payload : {};
+          res.writeHead(statusCode)
+          res.end(JSON.stringify(payload))
+       }
+     )
   })
   
 })
@@ -20,3 +39,23 @@ var server = http.createServer(function(req, res) {
 server.listen(3000, function() {
   console.log('listen on 3000')
 })
+
+
+var handlers = {};
+handlers.hello = function(data, callback) {
+  callback(200, {
+    'name': 'shuai',
+    'age': '22',
+    'gender': 'male',
+    'height': '6 inch',
+    'weight': '78kg',
+    'msg': 'hello, my name is shuai, nice to meet you'
+  });
+}
+handlers.notFound = function(data, callback) {
+  callback(404);
+}
+
+var routers = {
+  'hello': handlers.hello
+}
